4e86330ccf63edb6f3fa1225154a465d89de3e81
rakesh <rakeshshankar2k16@gmail.com>
2024-10-19 22:04:52 +0530

    wip2

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,20 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        charts: true,
+        reportPageTitle: 'customer-report',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: true,
+        reportFilename: "[status]_[datetime]-[name]-report",
+        timestamp: "longDate",
+        reportDir: "cypress/cypress-tests/reports/html",
+        reportTitle: "customer-report",
+        showSkipped: true,
+        showHooks: "always",
+    },
     e2e: {
         setupNodeEvents(on, config) {
             on("task", {
@@ -11,6 +25,10 @@ module.exports = defineConfig({
                     return null;
                 }
             });
+            require('cypress-terminal-report/src/installLogsPrinter')(on, {
+                printLogsToConsole: "always"
+            });
+            require('cypress-mochawesome-reporter/plugin')(on);
             return config;
         },
         env: {
@@ -18,6 +36,8 @@ module.exports = defineConfig({
             EMAIL: "",
             PASSWORD: ""
         },
-        screenshotsFolder: "cypress/cypress-tests"
+        screenshotsFolder: "cypress/cypress-tests/screenshots",
+        videosFolder: "cypress/cypress-tests/videos",
+        video: true,
     },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,21 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-mochawesome-reporter/register';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+require('cypress-terminal-report/src/installLogsCollector')(
+    {
+        collectTypes: [
+            "cons:log",
+            "cons:info",
+            "cons:warn",
+            "cons:error",
+            "cy:log",
+            "cy:request",
+            "cy:command",
+            // "cy:xhr"
+        ],
+    }
+);
